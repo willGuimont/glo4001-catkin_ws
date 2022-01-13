4c70715519e3b791535d94b58784c26e8ce738ef
William Guimont-Martin <wil9876543210@hotmail.com>
2022-01-13 12:25:00 -0500

    simple data transform

--- src/turtlebot3_simulations/turtlebot3_gazebo/scripts/range_sensor.py
+++ src/turtlebot3_simulations/turtlebot3_gazebo/scripts/range_sensor.py
@@ -5,13 +5,14 @@ from sensor_msgs.msg import LaserScan
 
 def make_callback(pub):
     def on_message(data):
+        data.ranges = [-x for x in data.ranges]
         rospy.loginfo(f'{rospy.get_caller_id()} Received {data.ranges[0]}')
-        pub.publish(f'Hello {data}')
+        pub.publish(data)
 
     return on_message
 
 if __name__ == '__main__':
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('/range/front', LaserScan, queue_size=10)
     rospy.init_node('range_sensor', anonymous=True)
 
     rospy.Subscriber('/proximity/front', LaserScan, make_callback(pub))
